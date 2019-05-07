54373dd577eb0c8b8966874f90bc3f34cdc022ab
carlo-mr <code@clietsch.de>
2019-05-07 18:46:39 +0200

    WIP: First steps to improve design

--- src/app/auth/containers/login.page.ts
+++ src/app/auth/containers/login.page.ts
@@ -5,13 +5,18 @@ import {LoginDetails} from '../models/auth.model';
 
 @Component({
   selector: 'auth-login-page',
+  styleUrls: ['login.page.scss'],
   template: `
+    <ion-header>
+      <img class="logo" src="./assets/img/splash-transparent.png"/>
+    </ion-header>
     <ion-content padding>
       <auth-login (login)="onLogin($event)"></auth-login>
-
+    </ion-content>
+    <ion-footer>
       <ion-button fill="outline" routerLink="/register" routerDirection="forward">Registrieren</ion-button>
       <ion-button fill="outline" routerLink="/forgot" routerDirection="forward">Passwort vergessen</ion-button>
-    </ion-content>
+    </ion-footer>
   `
 })
 export class LoginPage {
